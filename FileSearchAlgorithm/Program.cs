780d41368232bfb1c8c93d49af7831046660c15e
Codezone Corporation <dev@codezone.lk>
2018-03-29 18:38:21 +0530

    FileSearchAlgorithm First Commit

---
+++ FileSearchAlgorithm/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace FileSearchAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            var rootPath = @"F:\Folder"; // This is the path to root folder
+            FindFiles(rootPath);
+            Console.ReadLine();
+        }
+
+        public static void FindFiles(string rootPath)
+        {
+            int i = 0;
+            foreach (string fileName in Directory.GetFiles(rootPath))
+            {
+                Console.WriteLine(fileName);
+            }
+
+            foreach (string directory in Directory.GetDirectories(rootPath))
+            {
+                FindFiles(directory);
+            }
+        }
+    }
+}
